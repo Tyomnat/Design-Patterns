b286b18583ed6062ac983785a0f606a89a9a0e03
unblobfined <zilvinas.cep2@gmail.com>
2021-11-06 21:22:55 +0200

    Power ups: visuals

---
+++ ServerSide/Server/Powerups/Powerup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Powerups
+{
+    abstract class Powerup : MapObject
+    {
+        public string Type;
+        private Map Map;
+
+        public Powerup(string type, int Id, Map map) : base(Id)
+        {
+            Type = type;
+            this.Map = map;
+            int[] positions = SetPosition();
+            X = Map.Objects[positions[0]][positions[1]].X;
+            Y = Map.Objects[positions[0]][positions[1]].Y;
+            Map.Objects[positions[0]][positions[1]] = this;
+        }
+
+        private int[] SetPosition()
+        {
+            Random rnd = new Random();
+
+            int X = rnd.Next(0, Map.Objects.GetLength(0));
+            int Y = rnd.Next(0, Map.Objects[X].Length);
+
+            while (Map.Objects[X][Y].Id != 0)
+            {
+                X = rnd.Next(0, Map.Objects.GetLength(0));
+                Y = rnd.Next(0, Map.Objects[X].Length);
+            }
+
+            //Map.Objects[X][Y].Id = Id;
+
+
+            return new int[] { X, Y };
+        }
+    }
+}
---
+++ ServerSide/Server/Powerups/SpeedBoost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Powerups
+{
+    class SpeedBoost : Powerup
+    {
+        public SpeedBoost(Map map) : base("speedBoost", 503, map) { }
+    }
+}
